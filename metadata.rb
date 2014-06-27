d8da3f264f7770c96777d195a7ac6147476fd8e4
Phil Cohen <github@phlippers.net>
2014-06-26 18:16:15 -0700

    bump version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "github@phlippers.net"
 license          "MIT"
 description      "Chef cookbook to bootstrap an opinionated Ubuntu base image"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.0"
+version          "0.2.0"
 
 recipe "chef", "Configure Chef client"
 recipe "datadog", "Configure Datadog integration"
